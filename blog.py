ccb2a2c04d641b09c3732aa585918399838c3173
wedname <vvv.wedname@gmail.com>
2021-03-22 17:00:55 +0200

    Initial commit

--- blog.py
+++ blog.py
@@ -30,19 +30,6 @@ class Post:
         else:
             raise ValueError('description must be a string')
 
-    """
-    По заданию нужно было обрезать до 40 символов, но мне захотелось до 150.
-    Написал два варианта, не знаю какой лучше.
-    """
-    # def get_short_description(self):
-    #     data = self._description
-    #     if len(data) > 150:
-    #         data = data[:150].split()
-    #         data.pop()
-    #         return " ".join(data) + ".."
-    #     else:
-    #         return data
-
     def get_short_description(self):
         data = self._description
         if len(data) > 150:
@@ -67,15 +54,14 @@ class Blog:
         else:
             raise ValueError('posts_list must be a list')
 
-    @staticmethod
-    def add_post(posts_list) -> list:
-        return posts_list.append(Post)
+    def add_post(self, value_name: str, value_description: str):
+        return self._posts_list.append(Post(value_name, value_description))
 
-    def edit_post(self, value) -> list:
+    def edit_post(self, value, value_name: str, value_description: str) -> list:
         for i in range(0, len(self._posts_list)):
             if self._posts_list[i] == value:
-                self._posts_list[i].name()
-                self._posts_list[i].description()
+                self._posts_list[i].name(value_name)
+                self._posts_list[i].description(value_description)
                 self._posts_list[i].current_date = date.today()
             return self._posts_list[i]
         else:
